da53c7f6212ecd75d4a1ae9110a094ec7d75ef39
MariaPazCosta <mpazcostamagna@gmail.com>
2024-08-09 13:27:28 -0300

    ejercicios del 1 al 4 terminados, el de los arrays faltan 2 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let finalString=""
+    
+    if (num < 0){
+        finalString="ERROR"
+    }else{
+        for(let i = 0; i < num; i++){
+            finalString += string
+        }
+    }
+    
+    return finalString
 };
+repeatString("hey", 3)
+repeatString("hello", 10)
+repeatString("hi",1)
+repeatString("bye",0)
+repeatString("goodbye", -1)
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let reverse=""
+
+    for(let i= 1; i < string.length + 1; i++){
+        reverse+=string.at(-i)
+    }
+    return reverse
 };
-
+console.log(reverseString("hello"))
+console.log(reverseString("hello there"))
+console.log(reverseString("123! abc! Hello, Odinite."))
+console.log(reverseString(""))
 // Do not edit below this line
 module.exports = reverseString;
