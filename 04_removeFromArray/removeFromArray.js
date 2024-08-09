9e4b0a2a10441e9c9004e73357f84f48c60ef3e4
MariaPazCosta <mpazcostamagna@gmail.com>
2024-08-09 15:01:01 -0300

    complete los ultimos ejercicios de array pero le pregunté al chat, pido disculpas :8

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,39 +1,23 @@
 const removeFromArray = function(array=[],...removeItems) {
-    
-    let len= array.length
     let finalArray=[]
-    for( i =(len-1); i>= 0;i--){
-            
-        if(array[i] == firElement && secElement == ""){
-            
-            finalArray=array.splice((array.indexOf(firElement)),1)
-
-        }else if(array[i] == firElement || array[i]==secElement){
-            
-            let in1 =array.indexOf(firElement)
-            let in2=array.indexOf(secElement)
-            
-            if(in1 == -1 && in2 != -1){
-               finalArray= array.splice(in2,1)
-            }else if (in1 != -1 && in2 == -1){
-                finalArray=array.splice(in1,1)
-            }else{
-                finalArray = array.splice(in1,1)
-                finalArray = array.splice((in2-1),1)
-            }
-
-    }
-}
+    let items = removeItems
+    
+    finalArray = array.filter(element => !removeItems.includes(element));
     
     return finalArray
+}
 
-};
-// console.log(removeFromArray([1,2,3,4],3))
-// console.log(removeFromArray([1,2,3,4],3,2))
-// console.log(removeFromArray([1,2,2,4],2))
-// console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"))
-// console.log(removeFromArray([1, 2, 3, 4], 7, 2))
+    
+
+;
+ console.log(removeFromArray([1,2,3,4],3))
+ console.log(removeFromArray([1,2,3,4],3,2))
+  console.log(removeFromArray([1,2,2,4],2))
+ console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"))
+ console.log(removeFromArray([1, 2, 3, 4], 7, 2))
 console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4))
+console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3))
+console.log(removeFromArray([1, 2, 3], "1", 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
